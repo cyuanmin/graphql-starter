96d56430d8f2907102c78b73b424def8c48053b9
Yuanming Chen <yuanming@hotmail.com>
2017-10-23 17:50:14 -0600

    More advance graphQL to transform output

--- data/schema.js
+++ data/schema.js
@@ -9,53 +9,74 @@ import {
     GraphQLBoolean,
     GraphQLFloat
   } from 'graphql';
-
-  // Here is some dummy data to make this piece of code simpler.
-// It will be changeable after introducing mutation.
-var TODOs = [
-    {
-      "id": 1446412739542,
-      "title": "Read emails",
-      "completed": false
-    },
-    {
-      "id": 1446412740883,
-      "title": "Buy orange",
-      "completed": true
-    }
-  ];
-
-  const TodoType = new GraphQLObjectType({
-    name: 'todo',
-    fields: function () {
-      return {
-        id: {
-          type: GraphQLInt
-        },
-        title: {
-          type: GraphQLString
-        },
-        completed: {
-          type: GraphQLBoolean
+  
+  import axios from 'axios';
+  
+  let UserInfoType = new GraphQLObjectType({
+    name: "UserInfo",
+    description: "Basic information on a GitHub user",
+    fields: () => ({
+      "login": { type: GraphQLString },
+      "id": { type: GraphQLInt },
+      "avatar_url": { type: GraphQLString },
+      "gravatar_id": { type: GraphQLString },
+      "url": { type: GraphQLString },
+      "html_url": { type: GraphQLString },
+      "followers_url": { type: GraphQLString },
+      "following_url": {
+        type: GraphQLString,
+        resolve: (obj) => {
+          const brackIndex = obj.following_url.indexOf("{");
+          return obj.following_url.slice(0, brackIndex);
         }
-      }
-    }
+      },
+      "gists_url": { type: GraphQLString },
+      "starred_url": { type: GraphQLString },
+      "subscriptions_url": { type: GraphQLString },
+      "organizations_url": { type: GraphQLString },
+      "repos_url": { type: GraphQLString },
+      "events_url": { type: GraphQLString },
+      "received_events_url": { type: GraphQLString },
+      "type": { type: GraphQLString },
+      "site_admin": { type: GraphQLBoolean },
+      "name": { type: GraphQLString },
+      "company": { type: GraphQLString },
+      "blog": { type: GraphQLString },
+      "location": { type: GraphQLString },
+      "email": { type: GraphQLString },
+      "hireable": { type: GraphQLBoolean },
+      "bio": { type: GraphQLString },
+      "public_repos": { type: GraphQLInt },
+      "public_gists": { type: GraphQLInt },
+      "followers": { type: GraphQLInt },
+      "following": { type: GraphQLInt },
+      "created_at": { type: GraphQLString },
+      "updated_at": { type: GraphQLString },
+    })
   });
-
   
   const query = new GraphQLObjectType({
     name: "Query",
     description: "First GraphQL Server Config - Yay!",
-    fields: function(){
-        return {
-            todos:{
-                type: new GraphQLList(TodoType),
-                resolve: function(){
-                    return TODOs;
-                }
-            }
+    fields: () => ({
+      gitHubUser: {
+        type: UserInfoType,
+        description: "GitHub user API data with enhanced and additional data",
+        args: {
+          username: {
+            type: new GraphQLNonNull(GraphQLString),
+            description: "The GitHub user login you want information on",
+          },
+        },
+        resolve: (_,{username}) => {
+          const url = `https://api.github.com/users/${username}`;
+          return axios.get(url)
+                      .then(function(response) {
+                        return response.data;
+                      });
         }
-    }
+      },
+    })
   });
   
   const schema = new GraphQLSchema({
